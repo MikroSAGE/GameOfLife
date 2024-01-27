ebfb5b5b0237d3c1eea65b7f85ddf0918a94d184
maiken_dev <mikepengxinwei@gmail.com>
2024-01-27 10:05:56 -0500

    Resolved circular imports

--- cell.py
+++ cell.py
@@ -1,8 +1,6 @@
-from grid import Grid
 
 
 class Cell:
-
     x: int
     y: int
     alive: bool
@@ -12,21 +10,10 @@ class Cell:
         self.y = y
         self.alive = alive
 
-    def get_neighbour_count(self, grid: Grid) -> int:
-        n_count = 0
-        x, y = self.x, self.y
-        configs = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y),
-                   (x-1, y+1), (x, y+1), (x+1, y+1), (x+1, y)]
-        for j, i in configs:
-            if (0 <= i < grid.h and 0 <= j < grid.w) and grid.board[i][j].alive:
-                n_count += 1
-        return n_count
-
-    def update_stack(self, grid: Grid) -> None:
-        n_count = self.get_neighbour_count(grid)
+    def update(self, n_count: int, stack: list[tuple]) -> None:
         if self.alive:  # any living cell with less than 2 or more than 3 neighbours die
             if n_count < 2 or n_count > 3:
-                grid.stack.append((self.x, self.y))
+                stack.append((self.x, self.y))
         else:  # any dead cell with exactly 3 neighbours comes to life
             if n_count == 3:
-                grid.stack.append((self.x, self.y))
+                stack.append((self.x, self.y))
--- grid.py
+++ grid.py
@@ -18,10 +18,22 @@ class Grid:
         for i, j in loc:
             self.board[j][i].alive = not self.board[j][i].alive
 
+    def get_neighbour_count(self, cell: Cell) -> int:
+        n_count = 0
+        x, y = cell.x, cell.y
+        configs = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y),
+                   (x - 1, y + 1), (x, y + 1), (x + 1, y + 1), (x + 1, y)]
+        for j, i in configs:
+            if (0 <= i < self.h and 0 <= j < self.w) and self.board[i][j].alive:
+                n_count += 1
+        return n_count
+
     def update(self) -> None:
         for i in range(self.h):
             for j in range(self.w):
-                self.board[i][j].update_stack(self)
+                cell = self.board[i][j]
+                n_count = self.get_neighbour_count(cell)
+                cell.update(n_count, self.stack)
         while self.stack:
             x, y = self.stack.pop(0)
             self.board[y][x].alive = not self.board[y][x].alive
